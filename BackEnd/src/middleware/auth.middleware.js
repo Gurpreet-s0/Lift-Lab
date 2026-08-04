f0ed674e203890986657e91d990e07c23a7016e0
Gurpreet Singh <100257273+Gurpreet-s0@users.noreply.github.com>
2026-08-04 20:16:36 +0530

    test

--- BackEnd/src/middleware/auth.middleware.js
+++ BackEnd/src/middleware/auth.middleware.js
@@ -1,7 +1,17 @@
 const jwt = require("jsonwebtoken");
-
-async function identifyUser(req,res,next) {
+const app = require("../app")
+app.get("/test-cookie", (req, res) => {
+  res.cookie("test", "123", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+  res.json({ ok: true });
+});
+async function identifyUser(req, res, next) {
   const token = req.cookies.jwt_token
+  console.log(req.headers.origin);
+  console.log(req.headers.cookie);
 
   if (!token) {
     return res.status(404).json({
